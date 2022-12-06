5c8baeaa4f0e101d37df3b8908c9cf2caedc04b3
antonionovelli92 <anto.novelli@hotmail.it>
2022-12-06 15:48:11 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -8,59 +8,36 @@ console.log('JS OK')
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+const ticketElement = document.getElementById('ticket')
 
+//Info generics
 
-// 1. Chiedere quanti km si vogliono percorrere
-// 2. Chiedere l'eta del passeggero
-// 3. Calcolare il prezzo totale del viaggio (che si baserà sui km dei passeggeri)
-// 4. calcolare due tipologie di sconto:
-// 4a. 20% SE minori
-// 4b. 40% SE maggiori
-// 5. applicare le due tipologie di sconto:
-// 5a. 20% SE minori
-// 5b. 40% SE maggiori
+const kms = prompt('Quanti chilometri vuoi percorrere?', 100)
+const age = prompt('Quanti anni hai?', 35)
 
-// 5. Prezzo finale 
-
-const priceElement = document.getElementById('price')
-// ! console.log('priceElement')
-
-let priceKm = 0.21;
-
-
-// 1. Chiedere quanti km si vogliono percorrere 2. Chiedere l'eta del passeggero
-const userDistance = parseInt(prompt('Quanti km devi effettuare?', 35).trim());
-const userAge = parseInt(prompt('Quanti anni hai?', 30).trim());
-// ! console.log(userDistance, userAge)
-
-// 3. Calcolare il prezzo totale del viaggio (che si baserà sui km dei passeggeri)
-const priceTotal = (userDistance * priceKm);
-// !console.log(priceTotal.toFixed(2))
-
-// 4. Calcolare sconto del 20%
-let discountJunior = parseInt((priceTotal / 100) * 20);
-let juniorPrice = priceTotal - discountJunior;
-// !console.log(juniorPrice.toFixed(2))
-
-// 4. Calcolare sconto del 40%
-let discountSenior = parseInt((priceTotal / 100) * 40);
-let seniorPrice = priceTotal - discountSenior;
-// !console.log(seniorPrice.toFixed(2))
-
-
-//applicare lo sconto
-if (userAge >= 65) {
-    console.log(seniorPrice.toFixed(2))
-    priceElement.innerText = `Complimenti, il tuo prezzo finale sarà di ${seniorPrice.toFixed(2)}€ `
-
+//controlliamo se ci sono probelmi
+if (isNaN(kms) || isNaN(age) || kms <= 0 || age <= 0) {
+    alert('I dati inseriti non sono corretti')
 } else {
-    console.log(juniorPrice.toFixed(2))
-    priceElement.innerText = `Complimenti, il tuo prezzo finale sarà di ${juniorPrice.toFixed(2)}€ `
 
+    // Calcolo il prezzo base
+    const basePrice = kms * 0.21;
+    let finalPrice = basePrice;
+
+
+    let discount = null;
+
+    // verifico eventuali sconti
+    if (age >= 65) {
+        finalPrice *= 0.6;
+        discount = "40%";
+
+    } else if (age < 18) {
+        finalPrice *= 0.8;
+        discount = "20%";
+    }
+    ticketElement.innerText = finalPrice.toFixed(2) + "€";
 }
-
-
-
 
 
 
